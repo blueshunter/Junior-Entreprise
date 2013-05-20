f971f059041c9960f4ee3362e3d1d0a4c34bcaf8
thoretton <thoretton.edwin@gmail.com>
2013-05-20 18:43:47 +0200

    modification du header correction du footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,8 +4,8 @@
         <div class="navbar navbar-fixed-bottom">
             <center>- Site réalisé avec les frameworks Codeigniter et Bootstrap - licence MIT - Contact </center> 
         </div>
-        <script src="<?php echo base_url('bootstrap/js/jquery-1.9.1.js') ?>"></script>
-        <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/jquery-1.9.1.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 
     </body>
 </html>
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,30 +1,44 @@
 <?php 
 ?>
 
-<div class="navbar navbar-inner">
-    <div class="container">  
-        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </a>
+<div class="navbar">
+    <div class ="navbar-inner">
+        <a class="brand" href="#">Junior Entreprise</a>
+        <a class="brand" id="accroche" class="" href="#">test</a>
+     
+        <ul class="nav nav-tabs">
+            <li class="divider"></li>
+            <li>
+                <a href="#" title ="lien Accueil">Accueil</a>
+            </li>
+            <li class="divider"></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Etudiant <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a tabindex="-1" href="#">lien1</a></li>
+                    <li><a tabindex="-1" href="#">lien2</a></li>
+                    <li><a tabindex="-1" href="#">lien3</a></li>
+                    <li class="divider"></li>
+                    <li><a tabindex="-1" href="#">Connexion</a></li>
+                </ul >
+            </li>
+            <li class="divider"></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Entreprise <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a tabindex="-1" href="#">lien1</a></li>
+                    <li><a tabindex="-1" href="#">lien2</a></li>
+                    <li><a tabindex="-1" href="#">lien3</a></li>
+                    <li class="divider"></li>
+                    <li><a tabindex="-1" href="#">Connexion</a></li>
+                </ul >
+            </li>
+            <li class="divider"></li>
+            <li class="dropdown">
+                <a href="#" title ="lien admin">Administration</a>
+            </li>
+            <li class="divider"></li>
+        </ul>
    
-        <center>Junior Entreprise Home Page</center>
-        <div class="nav-collapse">
-            <ul class ="nav">
-                <li class="divider-vertical">
-                   - etudiant -
-                    <a href="test"></a>
-                </li>
-                <li class="divider-vertical">
-                    - entreprise -
-                    <a href="test"  title="entrprise"></a>
-                </li>
-                <li class="divider-vertical">
-                    - admin -
-                    <a href="test"  title="admin"></a>
-                </li>
-            </ul>
-        </div>
     </div>
 </div>
